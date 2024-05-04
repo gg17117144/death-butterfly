2bce909cbb2c2e7297b16ede007fc3d10c57e280
gg17117144 <gg17117144@gmail.com>
2024-05-05 05:54:21 +0800

    feat: 新增偵測是否有物件再去執行

--- Assets/Script/Fly/skill.cs
+++ Assets/Script/Fly/skill.cs
@@ -7,11 +7,11 @@ namespace Script.Fly
     public class Skill : MonoBehaviour
     {
         public float lifeTime = 3f;
-    
+
         public int skillID;
 
         public int damage;
-    
+
         Vector3 _moveDirection;
         public float speed = 0;
 
@@ -20,12 +20,13 @@ namespace Script.Fly
 
         private SpriteRenderer _spriteRenderer;
         private GameObject _player;
-    
+
         private bool _isPlayerHealth = false;
 
         private Animator _animator;
 
         private bool _isUsed = false;
+
         private void Awake()
         {
             _playerHeart = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHeart>();
@@ -42,7 +43,6 @@ namespace Script.Fly
 
             if (skillID == 2)
             {
-            
             }
         }
 
@@ -52,7 +52,7 @@ namespace Script.Fly
             //Debug.Log(isplayerHealth);
             //move();
             _spriteRenderer.sortingOrder = 1 - (int)_player.transform.position.y;
-        
+
             if (_isPlayerHealth)
             {
                 // 增加玩家的生命值，根据恢复速率和 Time.deltaTime 来计算
@@ -69,14 +69,14 @@ namespace Script.Fly
         {
             switch (skillID)
             {
-                case 0://普通子彈
+                case 0: //普通子彈
                     StartCoroutine(HurtMonster(monster));
                     Destroy(gameObject);
                     break;
-                case 1://熾熱
+                case 1: //熾熱
                     //monsterAI.isHurt(damage);
                     break;
-                case 2://生命
+                case 2: //生命
                     Debug.Log("有觸發到回復氧氣喔~");
                     lifeTime += 10;
                     damage = Random.Range(20, 31);
@@ -84,31 +84,32 @@ namespace Script.Fly
                     StartCoroutine(HurtMonster(monster));
                     _animator.Play("animaBom");
                     break;
-                case 3://氧氣
+                case 3: //氧氣
                     _playerHeart.healO2(damage);
                     _playerMove.startplayerSpeedUP(5f);
                     lifeTime = 5f;
                     break;
-                case 4://閃電
+                case 4: //閃電
                     break;
-                case 5://光明
+                case 5: //光明
                     break;
-                case 6://傳送
+                case 6: //傳送
                     break;
-                case 7://補血治癒圈 生命+(還有少東西)
+                case 7: //補血治癒圈 生命+(還有少東西)
                     _playerHeart.healO2(damage);
                     _isPlayerHealth = true;
                     break;
             }
             //Destroy(gameObject);
         }
+
         void Move()
         {
             lifeTime -= Time.deltaTime;
-        
+
             if (lifeTime <= 0)
             {
-                _moveDirection = new Vector2(0,0); // 停止移動
+                _moveDirection = new Vector2(0, 0); // 停止移動
                 Destroy(gameObject);
             }
             else if (lifeTime > 0.1f)
@@ -123,17 +124,17 @@ namespace Script.Fly
             {
                 UseSkill(other.gameObject);
             }
-        
+
             if (other.CompareTag("enemy"))
             {
                 UseSkill(other.gameObject);
-            
+
                 _moveDirection = new Vector2(0, 0); // 停止移動
             }
-        
+
             if (other.CompareTag("Object") && other.isTrigger == false)
             {
-                if (skillID != 3)   //不是氧氣蝴蝶
+                if (skillID != 3) //不是氧氣蝴蝶
                 {
                     _moveDirection = new Vector2(0, 0); // 停止移動
                     Destroy(gameObject);
@@ -145,7 +146,7 @@ namespace Script.Fly
         {
             if (other.CompareTag("Player"))
             {
-                if (skillID == 3 && !_isUsed)   //氧氣
+                if (skillID == 3 && !_isUsed) //氧氣
                 {
                     var random = Random.Range(0, 3);
                     if (random == 0)
@@ -155,8 +156,9 @@ namespace Script.Fly
                         UseSkill();
                     }
                 }
+
                 //isplayerHealth = true;
-                if (skillID == 7)   //生命(雙)
+                if (skillID == 7) //生命(雙)
                 {
                     // 增加玩家的生命值，根据恢复速率和 Time.deltaTime 来计算
                     lifeTime -= Time.deltaTime;
@@ -170,15 +172,15 @@ namespace Script.Fly
         {
             if (other.CompareTag("Player"))
             {
-                if (skillID == 7)   //生命(雙)
+                if (skillID == 7) //生命(雙)
                 {
                     UseSkill();
                     _isPlayerHealth = false;
                 }
             }
         }
-    
-    
+
+
         IEnumerator Heal(int healHp)
         {
             //UIControl.instance.DebugText("-回覆血量效果動畫");
@@ -189,14 +191,15 @@ namespace Script.Fly
                 _playerHeart.healHp(healHpNum);
                 yield return new WaitForSeconds(0.5f);
             }
+
             Destroy(gameObject);
         }
-    
+
         IEnumerator HurtMonster(GameObject monster)
         {
             //UIControl.instance.DebugText("-回覆血量效果動畫");
             var randomdamage = Random.Range(1, 5);
-        
+
             if (monster.CompareTag("boss"))
             {
                 if (skillID == 0)
@@ -205,32 +208,39 @@ namespace Script.Fly
 
                     if (randomLuck <= 0.94f)
                     {
-            
                     }
-                    else if(randomLuck <= 0.99)
+                    else if (randomLuck <= 0.99)
                     {
-                        randomdamage = Random.Range(50,101);
+                        randomdamage = Random.Range(50, 101);
                     }
-                    else if(randomLuck <= 1)
+                    else if (randomLuck <= 1)
                     {
                         randomdamage = 999;
                     }
                 }
-            
+
                 for (int i = 0; i < damage; i++)
                 {
-                    monster.GetComponent<BossAI>().damage(randomdamage,transform.position);
-                    yield return new WaitForSeconds(0.5f);
+                    if (monster != null)
+                    {
+                        monster.GetComponent<BossAI>().damage(randomdamage, transform.position);
+                        yield return new WaitForSeconds(0.5f);
+                    }
                 }
+
                 Destroy(gameObject);
             }
-            else if(monster.CompareTag("enemy"))
+            else if (monster.CompareTag("enemy"))
             {
                 for (int i = 0; i < damage; i++)
                 {
-                    monster.GetComponent<monsterAI>().isHurt(randomdamage);
-                    yield return new WaitForSeconds(0.5f);
+                    if (monster != null)
+                    {
+                        monster.GetComponent<monsterAI>().isHurt(randomdamage);
+                        yield return new WaitForSeconds(0.5f);
+                    }
                 }
+
                 Destroy(gameObject);
             }
         }
@@ -240,6 +250,5 @@ namespace Script.Fly
             GetComponent<SpriteRenderer>().sprite = null;
             GetComponent<Collider2D>().enabled = false;
         }
-    
     }
 }
